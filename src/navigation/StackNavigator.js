0a4edc726c6b385f044448068865e9f2ca991734
Alissa Mae Celeste <alissamaeceleste@gmail.com>
2023-11-16 22:45:57 +0800

    Modified StackNavigator.

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -3,6 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+
 //icons
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
